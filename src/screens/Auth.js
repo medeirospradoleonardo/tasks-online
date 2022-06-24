156e133676859ad4f63262db318f4266908a2b48
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-06-24 02:11:24 -0300

    Tela de Autenticação #04

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ImageBackground, Text, StyleSheet, View, TextInput, TouchableOpacity, Platform } from 'react-native'
+import { ImageBackground, Text, StyleSheet, View, TextInput, TouchableOpacity, Platform, Alert } from 'react-native'
 
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
@@ -11,11 +11,19 @@ export default class Auth extends Component {
         email: '',
         password: '',
         confirmPassword: '',
-        stageNew: true
+        stageNew: false
     }
-    
-    render(){
-        return(
+
+    signinOrSignup = () => {
+        if (this.state.stageNew) {
+            Alert.alert('Sucesso', 'Criar Conta')
+        } else {
+            Alert.alert('Sucesso', 'Logar')
+        }
+    }
+
+    render() {
+        return (
             <ImageBackground source={backgroundImage}
                 style={styles.background}>
                 <Text style={styles.title}>Tasks</Text>
@@ -23,7 +31,7 @@ export default class Auth extends Component {
                     <Text style={styles.subtitle}>
                         {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados'}
                     </Text>
-                    {this.state.stageNew && 
+                    {this.state.stageNew &&
                         <TextInput placeholder='Nome' value={this.state.name}
                             style={styles.input}
                             onChangeText={name => this.setState({ name: name })} />
@@ -34,12 +42,12 @@ export default class Auth extends Component {
                     <TextInput placeholder='Senha' value={this.state.password}
                         style={styles.input} secureTextEntry={true}
                         onChangeText={password => this.setState({ password: password })} />
-                    {this.state.stageNew && 
+                    {this.state.stageNew &&
                         <TextInput placeholder='Confirmação de Senha' value={this.state.confirmPassword}
                             style={styles.input} secureTextEntry={true}
                             onChangeText={confirmPassword => this.setState({ confirmPassword: confirmPassword })} />
                     }
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={this.signinOrSignup}>
                         <View style={styles.button}>
                             <Text style={styles.buttonText}>
                                 {this.state.stageNew ? 'Registrar' : 'Entrar'}
@@ -47,48 +55,54 @@ export default class Auth extends Component {
                         </View>
                     </TouchableOpacity>
                 </View>
+                <TouchableOpacity style={{ padding: 10 }}
+                    onPress={() => this.setState({ stageNew: !this.state.stageNew })} >
+                    <Text style={styles.buttonText}>
+                        {this.state.stageNew ? 'Já possui conta?' : 'Ainda não possui conta?'}
+                    </Text>
+                </TouchableOpacity>
             </ImageBackground>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    background:{
+    background: {
         flex: 1,
         width: '100%',
         alignItems: 'center',
         justifyContent: 'center'
     },
-    title:{
+    title: {
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.secondary,
         fontSize: 70,
         marginBottom: 10
     },
-    subtitle:{
+    subtitle: {
         fontFamily: commonStyles.fontFamily,
         color: '#FFF',
         fontSize: 20,
         textAlign: 'center',
         marginBottom: 10
     },
-    formContainer:{
+    formContainer: {
         backgroundColor: 'rgba(0, 0, 0, 0.8)',
         padding: 20,
         width: '90%'
     },
-    input:{
+    input: {
         marginTop: 10,
         backgroundColor: '#FFF',
         padding: Platform.OS == 'ios' ? 15 : 10
     },
-    button:{
+    button: {
         backgroundColor: '#080',
         marginTop: 10,
         padding: 10,
         alignItems: 'center'
     },
-    buttonText:{
+    buttonText: {
         fontFamily: commonStyles.fontFamily,
         color: '#FFF',
         fontSize: 20
